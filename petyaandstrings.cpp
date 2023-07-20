2777d27c33559cb45da4c6d6eee1ac89d989cce2
veradibu <veradibucchianico@gmail.com>
2023-07-20 13:36:57 -0300

    Correccion de ejercicio 112A

--- petyaandstrings.cpp
+++ petyaandstrings.cpp
@@ -2,26 +2,21 @@
 using namespace std;
 
 int main() {
-    string a,b; 
-    cin >> a;
-    cin >> b;
+    string s1, s2; 
+    cin >> s1;
+    cin >> s2;
 
-    string array_a = a.c_str();
-    string array_b = b.c_str();
+    for(int i=0; i<s1.size(); i++) {
+        int n1 = tolower(s1[i]);
+        int n2 = tolower(s2[i]);
 
-
-    for (int i = 0; i < array_a.length(); i++) {
-        for (int j = 0; j < array_b.length(); j++) {
-            if (tolower(array_a[i]) < tolower(array_b[j])) {
-                cout << -1 << endl;
-                return 0;
-            } else if (tolower(array_a[i]) > tolower(array_b[j])) {
-                cout << 1 << endl;
-                return 0;
-            } else {
-                continue;
-            }
-        } 
+        if (n1 < n2) {
+            cout << -1 << endl;
+            return 0;
+        } else if (n1 > n2) {
+            cout << 1 << endl;
+            return 0;
+        }
     }
     cout << 0 << endl;
 }
